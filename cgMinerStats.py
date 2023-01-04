1fbd1f5018f7d6b2ca2cc7edc34700939baef3d1
max21h <116381805+max21h@users.noreply.github.com>
2023-01-04 12:29:47 +0100

    Update cgMinerStats.py
    Parameters added to display data from CKPool

--- cgMinerStats.py
+++ cgMinerStats.py
@@ -1,15 +1,23 @@
-import socket
+import datetime
 import json
+import os
+import socket
+import subprocess
 import sys
 import time
-import subprocess
-import os
-
-from board import SCL, SDA
-import busio
-from PIL import Image, ImageDraw, ImageFont
-import adafruit_ssd1306
+from threading import Thread
 from time import strftime
+
+import adafruit_ssd1306
+import busio
+# Web Scraping Prerequisites
+import requests
+from board import SCL, SDA
+from PIL import Image, ImageDraw, ImageFont
+
+background_task_started = False
+line1 = "Fetching"
+line2 = "data..."
 
 class CgminerAPI(object):
     """ Cgminer RPC API wrapper. """
@@ -59,6 +67,29 @@ class CgminerAPI(object):
             return self.command(attr, arg)
         return out
 
+def getCKPoolData(url, bcAdress, workerName, hrOption, draw):
+    # Read JSON from URL
+    global background_task_started
+    global line1
+    global line2
+
+    page = requests.get(url)
+    data = page.json()
+
+    try:
+        if(workerName != ""):
+            workerData = [x for x in data["worker"] if x["workername"] == bcAdress + "." + workerName][0]
+        else:
+            workerData = data
+
+        line1 = str(workerData["hashrate" + hrOption])
+        line2 = str(workerData["bestever"])
+    except:
+        line1 = "Worker"
+        line2 = "not found!"
+    
+    background_task_started = False
+
 # Create the I2C interface.
 i2c = busio.I2C(SCL, SDA)
 
@@ -96,6 +127,7 @@ font = ImageFont.load_default()
 
 #CGMiner API
 cgminer = CgminerAPI()
+doCGMinerAPI = True
 
 #Load Image
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -105,15 +137,33 @@ try:
 except:
     withImage = False
 
+#Get given Parameters
+url = ""
+second = 0
+if len(sys.argv) > 1:
+    doCGMinerAPI = False
+    bcAdress = sys.argv[1]
+    workerName = ""
+    hrOption = "1hr"
+    url = "https://solo.ckpool.org/users/" + bcAdress
+    if len(sys.argv) > 2:
+        hrOption = sys.argv[2]
+    if len(sys.argv) > 3:
+        workerName = sys.argv[3]
+
+
 while True:
     # Get System Infos
     DateTime = strftime("%d.%m.%Y %H:%M:%S")
     cmd = "hostname -I | cut -d\' \' -f1"
     IP = subprocess.check_output(cmd, shell = True)
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True)
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    cmd = "free -h | awk 'NR==2{printf \"Mem: %s/%s %.1f%%\", $3,$2,($3/($2*1000))*100}'"
     MemUsage = subprocess.check_output(cmd, shell = True)
+    MemUsage = str(MemUsage,'utf-8')
+    MemUsage = MemUsage.replace("Mi","M")
+    MemUsage = MemUsage.replace("Gi","G")
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell = True)
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
@@ -123,29 +173,54 @@ while True:
     # Write System Infos
     draw.text((x, top), DateTime, font=font, fill=255)
     draw.text((x, top+8),  "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((x, top+16), str(CPU,'utf-8') + " " + str(temp,'utf-8') , font=font, fill=255)
-    draw.text((x, top+25), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((x, top+16), str(CPU,'utf-8') + "  " + str(temp,'utf-8') , font=font, fill=255)
+    draw.text((x, top+25), MemUsage, font=font, fill=255)
 
-    # Write CGMiner Stats        
-    try:
-        summary = cgminer.summary()
-        if withImage:
-            draw.bitmap((x, top+41), btcLogo, fill=1)
-            draw.text((x+25, top+38), "GH/s av: " + '%.2f' % (summary['SUMMARY'][0]['MHS av'] / 1000), font=font, fill=255)
-            draw.text((x+25, top+47), "BEST: " + str(summary['SUMMARY'][0]['Best Share']), font=font, fill=255)
-            draw.text((x+25, top+56), "BLK: " + str(summary['SUMMARY'][0]['Found Blocks']), font=font, fill=255)
-        else: #if btcLogo is not available
-            draw.text((x, top+38), "GH/s av: " + '%.2f' % (summary['SUMMARY'][0]['MHS av'] / 1000), font=font, fill=255)
-            draw.text((x, top+47), "BEST: " + str(summary['SUMMARY'][0]['Best Share']), font=font, fill=255)
-            draw.text((x, top+56), "BLK: " + str(summary['SUMMARY'][0]['Found Blocks']), font=font, fill=255)
-    except:
-        if withImage:
-            draw.bitmap((x, top+41), btcLogo, fill=1)
-            draw.text((x+25, top+44), "Check CGMiner", font=font, fill=255)
-            draw.text((x+25, top+52), "process !", font=font, fill=255)
-        else:
-            draw.text((x, top+44), "Check CGMiner", font=font, fill=255)
-            draw.text((x, top+52), "process !", font=font, fill=255)
+    # Write CGMiner Stats   
+    if doCGMinerAPI:     
+        try:
+            summary = cgminer.summary()
+
+            y = 0
+            if(withImage):
+                y = 25
+                draw.bitmap((x, top+41), btcLogo, fill=1)
+
+            draw.text((x+y, top+38), "GH/s av: " + '%.2f' % (summary['SUMMARY'][0]['MHS av'] / 1000), font=font, fill=255)
+            draw.text((x+y, top+47), "BEST: " + str(summary['SUMMARY'][0]['Best Share']), font=font, fill=255)
+            draw.text((x+y, top+56), "BLK: " + str(summary['SUMMARY'][0]['Found Blocks']), font=font, fill=255)
+
+        except:
+            if withImage:
+                draw.bitmap((x, top+41), btcLogo, fill=1)
+
+            draw.text((x+y, top+44), "Check CGMiner", font=font, fill=255)
+            draw.text((x+y, top+52), "process !", font=font, fill=255)
+    else:
+        #Write CKPool Stats
+        try:
+            if not background_task_started:
+                background_task_started = True
+                second = datetime.datetime.now().second
+                if url != "":
+                    t = Thread(target=getCKPoolData, args=(url, bcAdress, workerName, hrOption, draw))
+                    t.start()
+            
+            y = 0
+            if(withImage):
+                y = 25
+                draw.bitmap((x, top+41), btcLogo, fill=1)
+
+            draw.text((x+y, top+38), "HR " + hrOption + ": " + line1.replace("T", " T").replace("G", " G").replace("M", " M"), font=font, fill=255)
+            draw.text((x+y, top+47), "BEST: " + line2, font=font, fill=255)
+            draw.text((x+y, top+56), "Worker: " + str(workerName), font=font, fill=255)
+
+        except:
+            if withImage:
+                draw.bitmap((x, top+41), btcLogo, fill=1)
+
+            draw.text((x+y, top+44), "Check", font=font, fill=255)
+            draw.text((x+y, top+52), "parameters!", font=font, fill=255)
 
     disp.image(image)
     disp.show()
